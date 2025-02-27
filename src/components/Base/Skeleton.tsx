5f6839bf786bac6ed9f49a4f7365e667e8f6c807
16-work <soup16-work@outlook.com>
2025-02-27 15:18:53 +0800

    perf: <Skeleton>

--- src/components/Base/Skeleton.tsx
+++ src/components/Base/Skeleton.tsx
@@ -1,5 +1,26 @@
+'use client';
+
+import { DEFAULT_THEME } from '@/constants/common';
+import { store } from '@/store';
+import { useMemo } from 'react';
+
+/** Props */
+interface Props {
+  className: string;
+  skeletonType?: 'light' | 'dark';
+}
+
 /** Component */
-export const Skeleton = (props: { className: string }) => {
+export const Skeleton = (props: Props) => {
+  /** Retrieval */
+  const { theme } = store.global();
+
+  /** Params */
+  const skeletonType = useMemo(() => {
+    const type = (props.skeletonType ?? theme.search('light') !== -1) ? 'dark' : 'light';
+    return `skeleton-${type}`;
+  }, [props.skeletonType, theme]);
+
   /** Template */
-  return <div className={`skeleton ${props.className}`}></div>;
+  return <div className={`${skeletonType} ${props.className}`}></div>;
 };
